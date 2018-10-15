781a2a83ab3413e80650f1c8ba77bc5c1e05d28a
Daryl Cimafranca <cimafrancad45@gmail.com>
2018-10-15 12:54:17 -0400

    heroku deployed

--- server.js
+++ server.js
@@ -29,7 +29,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/VNNArticles", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost/VNNArticles", { useNewUrlParser: true });
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/VNNArticles";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 // Routes
 require("./routes/htmlRoutes.js")(app);
